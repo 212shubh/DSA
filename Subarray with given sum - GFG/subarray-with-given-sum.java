f6b2a79dd391d55bec4ec620bb915789960bb7c7
shubhanshu rai <98205875+212shubh@users.noreply.github.com>
2023-08-27 21:13:18 +0530

    Added solution - LeetHub

--- Subarray with given sum - GFG/subarray-with-given-sum.java
+++ Subarray with given sum - GFG/subarray-with-given-sum.java
@@ -36,40 +36,54 @@ class Main{
 // } Driver Code Ends
 
 
-
-
-
-
-
-
 class Solution
 {
     //Function to find a continuous sub-array which adds up to a given number.
     static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
     {
-        // Your code here
-            
-        long sum=0;
-        int start=0;
-        ArrayList<Integer> a = new ArrayList<Integer>();
-    
-        for(int end=0;end<n;end++)
+        int start = 0;
+        int last = 0;
+        boolean flag=false;
+        int currsum=0;
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        
+         //iterating over the array.
+        for(int i=0;i<n;i++)
         {
-            sum+=arr[end];
-            while(sum>s && start<end)
+            //storing sum upto current element.
+            currsum+= arr[i];
+            
+            //checking if current sum is greater than or equal to given number.
+            if(currsum>=s)
             {
-                sum-=arr[start];
-                start++;
-            }
-            if(sum==s)
-            {
-                a.add(start+1);
-                a.add(end+1);
-                return  a;
-            }
+                last=i;
+                //we start from starting index till current index and do the 
+                //excluding part while s(given number) < currsum.
+                while(s<currsum && start<last)
+                {
+                    //subtracting the element from left i.e., arr[start]
+                    currsum-= arr[start];
+                    ++start;
+                }
                 
+                //now if current sum becomes equal to given number, we store 
+                //the starting and ending index for the subarray.
+                if(currsum==s)
+                {
+                    res.add(start + 1);
+                    res.add(last + 1);
+                    
+                    //flag is set to true since subarray exists.
+                    flag = true;
+                    break;
+                }
+            }
         }
-        a.add(-1);
-        return a;
+        //if no subarray is found, we store -1 in result else the found indexes.
+        if (flag==false) {
+            res.add(-1);
+        } 
+        //returning the result.
+        return res;    
     }
 }
