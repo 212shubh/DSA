681e87f1d49aff5367bf944492c657213d702ecd
shubhanshu rai <98205875+212shubh@users.noreply.github.com>
2023-10-16 19:29:38 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Uncommon characters/uncommon-characters.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String A = sc.next();
+                    String B = sc.next();
+                    Solution ob = new Solution();
+                    System.out.println(ob.UncommonChars(A, B));
+                }
+        }
+}
+// } Driver Code Ends
+
+
+
+
+//User function Template for Java
+
+class Solution
+{
+    String UncommonChars(String A, String B)
+    {
+        // code here
+        int c1 [] = new int[26];
+        int c2 [] = new int[26];
+        for(int i =0; i<A.length();i++)
+        {
+            c1[A.charAt(i)-'a']++;
+        }
+        for(int i =0; i<B.length();i++)
+        {
+            c2[B.charAt(i)-'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<26;i++)
+        {
+            if((c1[i]==0&&c2[i]!=0)||( c1[i]!=0 &&c2[i]==0))
+              {
+                  sb.append((char)('a'+i));
+              }
+        }
+        if(sb.length()==0)
+           return "-1";
+        else
+           return sb.toString();
+      //  return (sb.length()==0) ? "-1" : sb.toString();
+    }
+}
