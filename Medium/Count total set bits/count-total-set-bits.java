8dcd499b87dd51f0aa6b56539f864abbab2cd757
shubhanshu rai <98205875+212shubh@users.noreply.github.com>
2023-11-18 12:02:36 +0530

    Updated Solution - GfG to GitHub

--- Medium/Count total set bits/count-total-set-bits.java
+++ Medium/Count total set bits/count-total-set-bits.java
@@ -12,18 +12,35 @@ import java.math.BigInteger;
 
 class Solution{
     
+    
     //Function to return sum of count of set bits in the integers from 1 to n.
     public static int countSetBits(int n){
     
         // Your code here
-        if(n==0) return 0;
-        int pow = 0, temp = n;
-        while(temp>1){
-            temp = temp >> 1;
-            pow++;
-        }
-        return (int)Math.pow(2, pow-1)*pow + n-(int)Math.pow(2, pow) + 1 + countSetBits(n-(int)Math.pow(2, pow));
+         //Ignoring 0 as all the bits are unset. 
+        n++;
+        int count=0;
         
+        //Counting set bits from 1 to n.
+        for( int x=2 ; x/2<n ; x=x*2 )
+        {
+            //Total count of pairs of 0s and 1s.
+            int quotient = n/x;
+            //quotient gives the complete count of pairs of 1s.
+            //Multiplying it with the (current power of 2)/2 will 
+            //give the count of 1s in the current bit.
+            count += quotient * x / 2;
+            
+            int remainder = n%x;
+            //If the count of pairs is odd then we add the remaining 1s 
+            //which could not be grouped together. 
+            if(remainder > x/2)
+                count += remainder - x/2;
+        }
+        
+        //returning count of set bits.
+        return count;
+
     }
 }
 
