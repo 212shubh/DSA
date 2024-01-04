12edb69a9e63b4c5a23dba50297de27bb24b23bd
shubhanshu rai <98205875+212shubh@users.noreply.github.com>
2024-01-04 23:05:04 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find element occuring once when all other are present thrice/find-element-occuring-once-when-all-other-are-present-thrice.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for JAVA
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            
+            int[] arr = new int[N];
+            
+            for(int i=0 ; i<N ; i++)
+                arr[i] = Integer.parseInt(S[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.singleElement(arr,N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int singleElement(int[] arr , int N) {
+        // code here
+         int one = 0;
+        int two = 0;
+        int bit;
+        for(int i = 0;i<N;i++){
+            two = two | (one & arr[i]);
+            one = one ^ arr[i];
+            bit = ~(one & two);
+            one = one & bit;
+            two = two & bit;
+        }
+        return one;
+    }
+}
